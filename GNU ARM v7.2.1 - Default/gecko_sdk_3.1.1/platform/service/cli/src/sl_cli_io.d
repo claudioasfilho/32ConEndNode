606d008c72bb8ea30de5fcd0f3adfd999e56d163
Claudio Filho <claudioasfilho@gmail.com>
2021-05-07 13:09:16 -0700

    first commit

---
+++ GNU ARM v7.2.1 - Default/gecko_sdk_3.1.1/platform/service/cli/src/sl_cli_io.d
@@ -0,0 +1,23 @@
+gecko_sdk_3.1.1/platform/service/cli/src/sl_cli_io.o: \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1/platform/service/cli/src/sl_cli_io.c \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/common/toolchain/inc/sl_gcc_preinclude.h \
+ /Users/clfilho/SimplicityStudio/May_21/soc_thermometer_mock/config/sl_cli_config.h \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1/platform/service/cli/src/sli_cli_io.h \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/service/iostream/inc/sl_iostream.h \
+ /Users/clfilho/SimplicityStudio/May_21/soc_thermometer_mock/autogen/sl_component_catalog.h \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/common/inc/sl_status.h \
+ /Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/common/inc/sl_enum.h
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/common/toolchain/inc/sl_gcc_preinclude.h:
+
+/Users/clfilho/SimplicityStudio/May_21/soc_thermometer_mock/config/sl_cli_config.h:
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1/platform/service/cli/src/sli_cli_io.h:
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/service/iostream/inc/sl_iostream.h:
+
+/Users/clfilho/SimplicityStudio/May_21/soc_thermometer_mock/autogen/sl_component_catalog.h:
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/common/inc/sl_status.h:
+
+/Applications/SimplicityStudio5.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.1//platform/common/inc/sl_enum.h:
